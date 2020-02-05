71378d557d643e8d184334a15ba511f1e449e443
Edynson Coronado <edynsoncoronado@gmail.com>
2020-02-05 09:44:51 -0500

    [REFACTOR] Modificado pg_dump

--- scripts/pg_dump-to-s3.sh
+++ scripts/pg_dump-to-s3.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#Creates a pgdump and uploads it to a s3 bucket
-# install with pip install awscli , apt install is not sure because dont create aws in /usr/local/bin/
-#usage: 
-#    pgdump-to-s3.sh [database_name] [bucket_folder] [bucket_name]
 
 DEFAULTBUCKETNAME=backupsrepos
 DEFAULTBUCKETFOLDER=test
@@ -10,11 +6,15 @@ DEFAULTDBNAME=dockert
 DEFAULTDUMPLOCATION=/tmp
 
 DBNAME="${1:-$DEFAULTDBNAME}"
-BUCKETFOLDER="${2:-$DEFAULTBUCKETFOLDER}"
-BUCKETNAME="${3:-$DEFAULTBUCKETNAME}"
+BUCKETNAME="${2:-$DEFAULTBUCKETNAME}"
+BUCKETFOLDER="${3:-$DEFAULTBUCKETFOLDER}"
 DUMPLOCATION=$DEFAULTDUMPLOCATION
-
+echo "DBNAME: $DBNAME"
+echo "BUCKETNAME: $BUCKETNAME"
+echo "BUCKETFOLDER: $BUCKETFOLDER"
+echo "DUMPLOCATION: $DUMPLOCATION"
 BDPASSWORD='****'
+DBUSER='postgres'
 
 DATE=$(date +%d%m%Y-%H-%M)
 DUMPNAME=$DBNAME-$DATE.sql
@@ -22,21 +22,22 @@ DUMPPATH=$DUMPLOCATION/$DUMPNAME
 GZPATH=$DUMPPATH.gz
 #added .gz at end to reflect that this is a gzipped file
 BUCKETURL=s3://$BUCKETNAME/$BUCKETFOLDER/$DUMPNAME.gz
-
-#Create postgres DB backup
-PGPASSWORD=$BDPASSWORD pg_dump -U postgres --no-owner -h x.x.x.x $DBNAME > $DUMPNAME
-sudo chown ubuntu:ubuntu $DUMPPATH
+echo "DATA: $DATE"
+echo "DUMPNAME: $DUMPNAME"
+echo "DUMPAHT: $DUMPPATH"
+echo "GZPATH: $GZPATH"
+echo "BUCKETURL: $BUCKETURL"
 echo "#############################################"
-echo "1. Backup $DBNAME sqldump for $DBNAME created"
+PGPASSWORD=$DBPASSWORD pg_dump -U $DBUSER --no-owner -h x.x.x.x $DBNAME > $DUMPPATH
+sudo chown ubuntu:ubuntu $DUMPPATH
+echo "1. Backup $DBNAME sqldump for $DBNAME created in $DUMPPATH"
 #Gzip file creates file with name $GZPATH
-gzip $DUMPPATH
+gzip -9 $DUMPPATH
 echo "2. Backup $DBNAME compressed with gzip"
 
-#Send backup to s3 bucket (aws previously configured)
 /usr/local/bin/aws s3 cp $GZPATH $BUCKETURL --acl public-read
 echo "3. Backup $DBNAME sqldump uploaded: $GZPATH to s3 bucket"
 
-#Delete created backup
 rm -f $GZPATH
-echo "4.Backup $DBNAME sqldump local file deleted "
+echo "4.Backup $DBNAME sqldump local file deleted"
 echo "############################################"
