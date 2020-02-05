ab1375f4245e92e46bcb9097076ee94822942289
Edynson Coronado <edynsoncoronado@gmail.com>
2020-02-05 17:39:20 -0500

    [FEAT] Parametros backup to s3

--- scripts/pg_dump-to-s3.sh
+++ scripts/pg_dump-to-s3.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
-
-DEFAULTBUCKETNAME=backupsrepos
+DEFAULTBUCKETNAME=backupsyaros
 DEFAULTBUCKETFOLDER=test
 DEFAULTDBNAME=dockert
 DEFAULTDUMPLOCATION=/tmp
 
-DBNAME="${1:-$DEFAULTDBNAME}"
-BUCKETNAME="${2:-$DEFAULTBUCKETNAME}"
-BUCKETFOLDER="${3:-$DEFAULTBUCKETFOLDER}"
+DBNAME=$(python3 -c "import base64; b = base64.b64decode(b'${1:-$DEFAULTDBNAME}').decode('utf-8'); print(b)")
+BUCKETNAME=$(python3 -c "import base64; b = base64.b64decode(b'${2:-$DEFAULTBUCKETNAME}').decode('utf-8'); print(b)")
+BUCKETFOLDER=$(python3 -c "import base64; b = base64.b64decode(b'${3:-$DEFAULTBUCKETFOLDER}').decode('utf-8'); print(b)")
 DUMPLOCATION=$DEFAULTDUMPLOCATION
+DBUSER=$(python3 -c "import base64; b = base64.b64decode(b'$4').decode('utf-8'); print(b)")
+DBPASSWORD=$(python3 -c "import base64; b = base64.b64decode(b'$5').decode('utf-8'); print(b)")
+SERVERIP=$(python3 -c "import base64; b = base64.b64decode(b'$6').decode('utf-8'); print(b)")
+
 echo "DBNAME: $DBNAME"
 echo "BUCKETNAME: $BUCKETNAME"
 echo "BUCKETFOLDER: $BUCKETFOLDER"
 echo "DUMPLOCATION: $DUMPLOCATION"
-BDPASSWORD='****'
-DBUSER='postgres'
+echo "DBUSER: $DBUSER"
+echo "DBPASSWORD: $DBPASSWORD"
 
 DATE=$(date +%d%m%Y-%H-%M)
 DUMPNAME=$DBNAME-$DATE.sql
 DUMPPATH=$DUMPLOCATION/$DUMPNAME
 GZPATH=$DUMPPATH.gz
-#added .gz at end to reflect that this is a gzipped file
 BUCKETURL=s3://$BUCKETNAME/$BUCKETFOLDER/$DUMPNAME.gz
 echo "DATA: $DATE"
 echo "DUMPNAME: $DUMPNAME"
@@ -28,10 +30,11 @@ echo "DUMPAHT: $DUMPPATH"
 echo "GZPATH: $GZPATH"
 echo "BUCKETURL: $BUCKETURL"
 echo "#############################################"
-PGPASSWORD=$DBPASSWORD pg_dump -U $DBUSER --no-owner -h x.x.x.x $DBNAME > $DUMPPATH
+
+PGPASSWORD=$DBPASSWORD pg_dump -U $DBUSER --no-owner -h $SERVERIP $DBNAME > $DUMPPATH
 sudo chown ubuntu:ubuntu $DUMPPATH
 echo "1. Backup $DBNAME sqldump for $DBNAME created in $DUMPPATH"
-#Gzip file creates file with name $GZPATH
+
 gzip -9 $DUMPPATH
 echo "2. Backup $DBNAME compressed with gzip"
 
